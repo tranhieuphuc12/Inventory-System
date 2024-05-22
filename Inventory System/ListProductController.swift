7003f7742c9c1f2edf3962ad11f271c864a9b0cd
charcasbeos <tranthianhthu0711@gmail.com>
2024-05-22 16:07:45 +0700

    storage ui, update storage ui, image picker, fmdb

---
+++ Inventory System/ListProductController.swift
@@ -0,0 +1,12 @@
+//
+//  ListProductController.swift
+//  Inventory System
+//
+//  Created by tran thu on 20/05/2024.
+//
+
+import UIKit
+class ListProductController:UIViewController{
+    @IBOutlet weak var navigation: UINavigationBar!
+    
+}
---
+++ Inventory System/StorageTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  StorageTableViewController.swift
+//  Inventory System
+//
+//  Created by tran thu on 20/05/2024.
+//
+
+import UIKit
+
+class StorageTableViewController: UITableViewController, UITabBarControllerDelegate{
+
+    
+    @IBOutlet weak var navigation: UINavigationItem!
+    
+    @IBOutlet weak var delete: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigation.setRightBarButton(delete, animated: true)
+        navigation.rightBarButtonItem = editButtonItem
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    override func viewWillAppear(_ animated: Bool) {
+           super.viewWillAppear(animated)
+           // Thực hiện các thiết lập hoặc cập nhật cần thiết mỗi khi view xuất hiện
+           print("viewWillAppear called")
+       }
+
+       override func viewDidAppear(_ animated: Bool) {
+           super.viewDidAppear(animated)
+           // Thực hiện các thiết lập hoặc cập nhật cần thiết mỗi khi view đã xuất hiện
+           print("viewDidAppear called")
+       }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        // Tao doi tuong man hinh MealDetailController
+        if let updateStorage = self.storyboard!.instantiateViewController(withIdentifier: "UpdateStorage") as? UpdateStorageController {
+            //Danh dau trang thai
+            
+            //Hien thi man hinh meal detail controller
+            present(updateStorage, animated: true)
+            
+        }
+        
+    }
+    @IBAction func unwindFromUpdateStorage(_ segue:UIStoryboardSegue){
+        if segue.source is UpdateStorageController{
+            print("Unwind segue performed")
+        }
+    }
+
+}
