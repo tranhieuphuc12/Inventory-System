4c55589c0cf14a476d2a20d3422bc364b00a506e
charcasbeos <tranthianhthu0711@gmail.com>
2024-05-23 11:27:42 +0700

    Done Products and ImportExport table, func insert

--- Inventory System/FMDB/DAL/Database.swift
+++ Inventory System/FMDB/DAL/Database.swift
@@ -18,8 +18,23 @@ class Database {
     
     //MARK: Cac thuoc tinh lien quan den bang du lieu
     //1.Product
-    
-    
+    private let PRODUCT_TABLE_NAME = "products"
+    private let PRODUCT_ID = "_id"
+    private let PRODUCT_NAME = "name"
+    private let PRODUCT_IMAGE = "image"
+    private let PRODUCT_UNIT = "unit"
+    private let PRODUCT_PROFIT = "profit"
+    private let PRODUCT_QUANTITY = "quantity" //So luong ton kho
+    private let PRODUCT_COST = "cost" //Gia cost trung binh
+    //2.ImportExport
+    private let IMPORTEXPORT_TABLE_NAME = "import_export"
+    private let IMPORTEXPORT_ID = "_id"
+    private let IMPORTEXPOR_PRODUCTID = "productID"
+    private let IMPORTEXPOR_QUANTITY = "quantity"
+    private let IMPORTEXPOR_COST = "cost"
+    private let IMPORTEXPOR_DATE = "date"
+
+        
     //MARK: Constructor
     init(){
         //Lay tat ca duong dan den cac thu muc doccument trong mot ung dung iOS
@@ -32,18 +47,46 @@ class Database {
         if database != nil {
             os_log("Tao CSDL thanh cong !")
             //Thuc hien tao bang du lieu
+            //1.Products
+            let sql_products = "Create table \(PRODUCT_TABLE_NAME)("
+            + "\(PRODUCT_ID) INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "\(PRODUCT_NAME) TEXT ,"
+            + "\(PRODUCT_IMAGE) TEXT ,"
+            + "\(PRODUCT_UNIT) TEXT ,"
+            + "\(PRODUCT_PROFIT) DOUBLE,"
+            + "\(PRODUCT_QUANTITY) INTEGER,"
+            + "\(PRODUCT_COST) DOUBLE)"
+            //2.ImportExport
+            let sql_import_export = "Create table \(IMPORTEXPORT_TABLE_NAME)("
+            + "\(IMPORTEXPORT_ID) INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "\(IMPORTEXPOR_PRODUCTID) INTEGER ,"
+            + "\(IMPORTEXPOR_QUANTITY) INTEGER ,"
+            + "\(IMPORTEXPOR_COST) DOUBLE ,"
+            + "\(IMPORTEXPOR_DATE) DEFAULT CURRENT_DATE)"
             
 
-//            if open(){
-//                if !database!.tableExists(PRODUCT_TABLE_NAME){
-//                    let _ = createTable(sql: sql)
-//                }
-//            }
+            if open(){
+                if !database!.tableExists(PRODUCT_TABLE_NAME){
+                    if createTable(sql: sql_products){
+                        os_log("Tao bang product thanh cong")
+                    }
+                    else{
+                        os_log("Tao bang product khong thanh cong")
+                    }
+                }
+                if !database!.tableExists(IMPORTEXPORT_TABLE_NAME){
+                    if createTable(sql: sql_import_export){
+                        os_log("Tao bang importexport thanh cong")
+                    }else{
+                        os_log("Tao bang importexport khong thanh cong")
+                    }
+                }
+            }
            
             
         }
         else{
-            os_log("Tao CSDL that bai !")
+            os_log("Tao CSDL khong thanh cong !")
         }
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -58,7 +101,7 @@ class Database {
                 OK = true
             }
             else{
-                os_log("Mo CSDL that bai")
+                os_log("Mo CSDL khong thanh cong")
             }
         }
         
@@ -75,49 +118,69 @@ class Database {
     private func createTable(sql:String)->Bool{
         var OK = false
         if database!.executeStatements(sql){
-            os_log("Tao bang thanh cong")
             OK = true
         }
-        else{
-            os_log("Tao bang that bai")
-        }
-        
-        
         return OK
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //MARK: Dinh nghia cac ham APIs cua CSDL
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //1. them 1 meal vao CSDL
-//    func insert(product:Product)->Bool{
-//        var OK = false
-//        if open(){
-//            //Kiem tra su ton tai cua bang du lieu
-//            if  database!.tableExists(PRODUCT_TABLE_NAME){
-//                //Cau lenh sql de them du lieu vao CSDL
-//                let sql = "INSERT INTO \(PRODUCT_TABLE_NAME) "
-//                + "(\(PRODUCT_NAME),\(PRODUCT_IMAGE),\(PRODUCT_UNIT),\(PRODUCT_COST),\(PRODUCT_PROFIT),\(PRODUCT_PRICE),\(PRODUCT_QUANTITY)) VALUES (?,?,?,?,?,?,?)"
-//                //Chuyen UIImage thanh chuoi
-//                var strImage = ""
-//                if let image = product.image{
-//                    //B1. Chuyen anh thanh NSData
-//                    let nsdataImage = image.pngData()! as NSData
-//                    //B2. Chuyen nsdataImage thanh chuoi
-//                    strImage = nsdataImage.base64EncodedString(options: .lineLength64Characters)
-//                }
-//                //Luu gia tri meal vao CSDL
-//                if database!.executeUpdate(sql, withArgumentsIn: [product.name, strImage, product.unit, product.cost, product.profit,product.price,product.quantity]){
-//                    os_log("Them du lieu thanh cong")
-//                    OK = true
-//                    //Dong co so du lieu
-//                    close()
-//                }
-//                else{
-//                    os_log("Them du lieu that bai")
-//                }
-//            }
-//            
-//        }
-//        return OK
-//    }
+    //1. Insert
+    ///1.1 Products
+    func insertProduct(product:Product)->Bool{
+        var OK = false
+        if open(){
+            //Kiem tra su ton tai cua bang du lieu
+            if  database!.tableExists(PRODUCT_TABLE_NAME){
+                //Cau lenh sql de them du lieu vao CSDL
+                let sql = "INSERT INTO \(PRODUCT_TABLE_NAME) "
+                + "(\(PRODUCT_NAME),\(PRODUCT_IMAGE),\(PRODUCT_UNIT),\(PRODUCT_PROFIT),\(PRODUCT_QUANTITY),\(PRODUCT_COST)) VALUES (?,?,?,?,?,?)"
+                //Chuyen UIImage thanh chuoi
+                var strImage = ""
+                if let image = product.image{
+                    //B1. Chuyen anh thanh NSData
+                    let nsdataImage = image.pngData()! as NSData
+                    //B2. Chuyen nsdataImage thanh chuoi
+                    strImage = nsdataImage.base64EncodedString(options: .lineLength64Characters)
+                }
+                //Luu gia tri meal vao CSDL
+                if database!.executeUpdate(sql, withArgumentsIn: [product.name, strImage, product.unit, product.profit,product.quantity, product.cost]){
+                    os_log("Them du lieu product thanh cong")
+                    OK = true
+                    //Dong co so du lieu
+                    close()
+                }
+                else{
+                    os_log("Them du lieu product khong thanh cong")
+                }
+            }
+            
+        }
+        return OK
+    }
+    ///1.2 ImportExport
+    func insertImportExport(importexport:ImportExport)->Bool{
+        var OK = false
+        if open(){
+            //Kiem tra su ton tai cua bang du lieu
+            if  database!.tableExists(PRODUCT_TABLE_NAME){
+                //Cau lenh sql de them du lieu vao CSDL
+                let sql = "INSERT INTO \(IMPORTEXPORT_TABLE_NAME) "
+                + "(\(IMPORTEXPOR_PRODUCTID),\(IMPORTEXPOR_QUANTITY),\(IMPORTEXPOR_COST),\(IMPORTEXPOR_DATE)) VALUES (?,?,?,?)"
+                
+                //Luu gia tri meal vao CSDL
+                if database!.executeUpdate(sql, withArgumentsIn: [importexport.productID, importexport.quantity, importexport.cost, importexport.date]){
+                    os_log("Them du lieu importexport thanh cong")
+                    OK = true
+                    //Dong co so du lieu
+                    close()
+                }
+                else{
+                    os_log("Them du lieu importexport khong thanh cong")
+                }
+            }
+            
+        }
+        return OK
+    }
 }
